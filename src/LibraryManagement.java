f8dd257e1555026049af3b57b4f8f323578d5d6c
NadaAlaa299 <nadaalaa2909@gmail.com>
2025-01-09 16:42:11 +0200

    update Library management class & write code in main class

--- src/LibraryManagement.java
+++ src/LibraryManagement.java
@@ -2,34 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LibraryManagement {
-    private List<Book> bookList = new ArrayList<>();
+    private List<Book> booksList = new ArrayList<>();
 
     public void addBook(Book book)
     {
-        bookList.add(book);
+        booksList.add(book);
     }
 
-    public void viewBook()
+    public void viewBooks()
     {
-        System.out.println("Available Books: ");
-        for (Book book : bookList) {
-            System.out.println("Book ID: "+ book.getId());
-            System.out.println("Book Name: "+ book.getTitle());
-            System.out.println("Author Name: "+ book.getAuthor());
-            System.out.println();
+        if (booksList.isEmpty())
+        {
+            System.out.println("No books in the library.");
         }
+        else
+        {
+            System.out.println("Available Books: ");
+            for (Book book : booksList) {
+                System.out.println("Book ID: "+ book.getId());
+                System.out.println("Book Name: "+ book.getTitle());
+                System.out.println("Author Name: "+ book.getAuthor());
+                System.out.println();
+            }
+        }
+
     }
 
     public void searchBook(String bookTitle)
     {
-        for (Book book : bookList) {
+        boolean found = true;
+        for (Book book : booksList) {
             if (book.getTitle().equalsIgnoreCase(bookTitle))
             {
+                found = false;
+                System.out.println("Book found");
                 System.out.println("Book ID: "+ book.getId());
                 System.out.println("Book Name: "+ book.getTitle());
                 System.out.println("Author Name: "+ book.getAuthor());
                 break;
             }
+        }
+        if (found)
+        {
+            System.out.println("Book not found");
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        LibraryManagement manager = new LibraryManagement();
+
+        while (true)
+        {
+            System.out.println("\n< Library Management System >");
+            System.out.println("1) Add Book");
+            System.out.println("2) View Books");
+            System.out.println("3) Search Book by Title");
+            System.out.println("4) Exit");
+            System.out.print("Enter your choice: ");
+
+            try {
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter Book ID: ");
+                        int id = scanner.nextInt();
+                        scanner.nextLine();
+                        System.out.print("Enter Book Title: ");
+                        String bookTitle = scanner.nextLine();
+                        System.out.print("Enter Author Name: ");
+                        String author = scanner.nextLine();
+
+                        Book book = new Book(id, bookTitle, author);
+                        manager.addBook(book);
+                        System.out.println("Book added successfully");
+                        break;
+                    case 2:
+                        manager.viewBooks();
+                        break;
+                    case 3:
+                        System.out.print("Enter Book Title: ");
+                        bookTitle = scanner.nextLine();
+                        manager.searchBook(bookTitle);
+                        break;
+                    case 4:
+                        System.out.println("Exiting...");
+                        System.exit(0);
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            } catch (Exception e)
+            {
+                System.out.println("An error occurred: " + e.getMessage());
+            }
+        }
     }
 }
